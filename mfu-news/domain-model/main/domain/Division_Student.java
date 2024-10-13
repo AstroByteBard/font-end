5bbc02e9e8f9a72eacc2b94da7ff5f85920c82b1
AstroByteBard <156665491+AstroByteBard@users.noreply.github.com>
2024-10-13 03:39:18 +0000

    fuck spring boot

---
+++ mfu-news/domain-model/main/domain/Division_Student.java
@@ -0,0 +1,11 @@
+
+
+
+
+
+
+
+
+public class Division_Student {
+    
+}
---
+++ mfu-news/src/test/java/com/mfu/mfu_news/MfuNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mfu.mfu_news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MfuNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
